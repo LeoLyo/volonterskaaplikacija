e5195e84d14c88cdde193a41087a1a2f95490e1e
LeoLyo <7leolyo7@gmail.com>
2018-04-09 22:57:11 +0200

    Added user registration.

--- app/src/main/java/com/random/anagnosti/volonterskaaplikacija/RegisterActivity.java
+++ app/src/main/java/com/random/anagnosti/volonterskaaplikacija/RegisterActivity.java
@@ -1,13 +1,125 @@
 package com.random.anagnosti.volonterskaaplikacija;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText editTextEmail, editTextFirstName, editTextLastName, editTextAddress, editTextPhoneNumber, editTextPassword, editTextConfirmPassword;
+    ProgressBar progressBarRegister;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextFirstName = findViewById(R.id.editTextFirstName);
+        editTextLastName = findViewById(R.id.editTextLastName);
+        editTextAddress = findViewById(R.id.editTextAddress);
+        editTextPhoneNumber = findViewById(R.id.editTextPhoneNumber);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword);
+        progressBarRegister = findViewById(R.id.progressBarRegister);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+
+    public void registerUser(View view){
+        String email = editTextEmail.getText().toString().trim();
+        String firstName = editTextFirstName.getText().toString().trim();
+        String lastName = editTextLastName.getText().toString().trim();
+        String address = editTextAddress.getText().toString().trim();
+        String phoneNumber = editTextPhoneNumber.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        String confirmPassword = editTextConfirmPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(firstName.isEmpty()){
+            editTextFirstName.setError("First name is required");
+            editTextFirstName.requestFocus();
+            return;
+        }
+        if(lastName.isEmpty()){
+            editTextLastName.setError("Last name is required");
+            editTextLastName.requestFocus();
+            return;
+        }
+
+        if(address.isEmpty()){
+            editTextAddress.setError("Address is required");
+            editTextAddress.requestFocus();
+            return;
+        }
+        if(phoneNumber.isEmpty()){
+            editTextPhoneNumber.setError("Phone number is required");
+            editTextPhoneNumber.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(password.length()<7){
+            editTextPassword.setError("Minimum length of password should be 7");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(confirmPassword.isEmpty()){
+            editTextConfirmPassword.setError("Confirming the password is required");
+            editTextConfirmPassword.requestFocus();
+            return;
+        }
+        if(!confirmPassword.matches(password)){
+            editTextConfirmPassword.setError("Confirmation of password must match the password");
+            editTextConfirmPassword.requestFocus();
+            return;
+        }
+
+        progressBarRegister.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBarRegister.setVisibility(View.GONE);
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"User Registered Succesfully", Toast.LENGTH_SHORT).show();
+                }else{
+                    if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                        Toast.makeText(getApplicationContext(),"This email is already registered",Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+
+
     }
 }
