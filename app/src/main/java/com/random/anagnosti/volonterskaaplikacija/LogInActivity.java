6c57c059850797fbb2b61dc3fc6242c32942f1f2
LeoLyo <7leolyo7@gmail.com>
2018-04-13 14:43:07 +0200

    Fixed login exiting problem and implemented sign out option.

--- app/src/main/java/com/random/anagnosti/volonterskaaplikacija/LogInActivity.java
+++ app/src/main/java/com/random/anagnosti/volonterskaaplikacija/LogInActivity.java
@@ -3,7 +3,6 @@ package com.random.anagnosti.volonterskaaplikacija;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
@@ -16,7 +15,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends Activity {
+public class LogInActivity extends Activity {
 
     FirebaseAuth mAuth;
     EditText editTextEmailLogin, editTextPasswordLogin;
@@ -73,9 +72,9 @@ public class MainActivity extends Activity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 progressBarLogin.setVisibility(View.GONE);
                 if(task.isSuccessful()){
-                    Intent intent2 = new Intent(MainActivity.this, WelcomeActivity.class);
-                    intent2.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(intent2);
+                    //Vraca WelcomActivity rezultat
+                    Intent returnIntent = new Intent();
+                    setResult(Activity.RESULT_OK, returnIntent);
                     finish();
                 }else{
                     Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/random/anagnosti/volonterskaaplikacija/WelcomeActivity.java
+++ app/src/main/java/com/random/anagnosti/volonterskaaplikacija/WelcomeActivity.java
@@ -1,18 +1,28 @@
 package com.random.anagnosti.volonterskaaplikacija;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class WelcomeActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class WelcomeActivity extends Activity {
+
+    public static final int RC_SIGN_IN = 1;
 
     ListView listView;
     String[] peopleList = {"Osoba 1", "Osoba2", "Osoba3", "Osoba4"};
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +31,9 @@ public class WelcomeActivity extends AppCompatActivity {
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.activity_listview,R.id.textView,peopleList);
         listView.setAdapter(arrayAdapter);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        initializeAuthListener();
     }
 
 
@@ -38,4 +51,60 @@ public class WelcomeActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CreateEventActivity.class);
         startActivity(intent);
     }
+
+    //    LIESTENER ZA LOGINOVANJE
+    private void initializeAuthListener(){
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull final FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if(user != null){
+
+//                    TESTIRANJE ZA SIGNOUT SAMO
+                    Button bt = findViewById(R.id.showItems);
+
+                    bt.setText(user.getEmail());
+
+                    bt.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            firebaseAuth.signOut();
+                        }
+                    });
+                }else{
+                    Intent intent = new Intent(getApplicationContext(), LogInActivity.class);
+                    startActivityForResult(intent, 1);
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        firebaseAuth.removeAuthStateListener(authStateListener);
+    }
+
+    //    OKIDA SE KAD LOGIN VRATI SA LOG IN ACTIVITY
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK)
+                Toast.makeText(this, "Signed in!", Toast.LENGTH_SHORT).show();
+            else if(resultCode == RESULT_CANCELED){
+                Toast.makeText(this, "Sign in canceled", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
 }
+
